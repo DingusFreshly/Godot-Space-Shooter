d6180582d4c2a06dc798da1a29ddfca97fc1c2b2
GazarTheBazar <gashomo6@gmail.com>
2026-04-13 18:43:21 -0500

    finnished ship and added skybox

---
+++ asteroid-shooter/Camera.gd
@@ -0,0 +1,13 @@
+extends Camera3D
+
+@onready var target = get_parent().get_node("Player")
+@export var follow_speed := 5.0
+@export var offset := Vector3(0, 4.5, 15)
+
+func _process(delta):
+	if target:
+		# Offset relative to player's rotation
+		var desired_position = target.global_transform.origin + target.global_transform.basis * offset
+		
+		global_position = global_position.lerp(desired_position, follow_speed * delta)
+		global_transform.basis = global_transform.basis.slerp(target.global_transform.basis, follow_speed * delta)
