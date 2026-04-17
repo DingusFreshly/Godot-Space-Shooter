7aed0a45f89a3e915b8cd1ec40624e5e5046affd
GazarTheBazar <gashomo6@gmail.com>
2026-04-17 13:36:30 -0500

    Added shooting and better control

---
+++ asteroid-shooter/scenes/Bullet.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+@export var speed := 100.0
+@export var lifetime := 2.0   # seconds
+
+func _ready():
+	await get_tree().create_timer(lifetime).timeout
+	queue_free()
+
+func _physics_process(delta):
+	position += -transform.basis.z * speed * delta
--- asteroid-shooter/scripts/Movement.gd
+++ asteroid-shooter/scripts/Movement.gd
@@ -7,6 +7,9 @@ var roll_speed = 1.9
 var yaw_speed = 1.25
 var input_response = 7.0
 
+var shoot_timer = 0
+var shoot_at = 0.05
+
 @export var forward_speed = 0.0
 var vel = Vector3.ZERO
 var pitch_input = 0.0
@@ -15,6 +18,12 @@ var yaw_input = 0.0
 
 @onready var left_thruster = $LeftThruster
 @onready var right_thruster = $RightThruster
+
+@onready var right_gun = get_node("RightGun")
+@onready var left_gun = get_node("LeftGun")
+var bullet_tick = 9
+
+@export var bullet_scene: PackedScene
 
 func handle_acceleration(delta):
 	
@@ -31,8 +40,6 @@ func handle_acceleration(delta):
 		right_thruster.emitting = false
 
 	forward_speed = lerp(forward_speed, target, acceleration * delta)
-		
-	forward_speed = lerp(forward_speed, target, acceleration * delta)
 	
 func handle_rotation(delta):
 	
@@ -40,10 +47,41 @@ func handle_rotation(delta):
 	roll_input = lerp(roll_input, Input.get_action_strength("left") - Input.get_action_strength("right"), input_response * delta)
 	yaw_input = roll_input #lerp(yaw_input, Input.get_action_strength("yaw_right") - Input.get_action_strength("yaw_left"), input_response * delta)
 
+func get_shoot_transform(delta):
+	var gun: Node3D
+	
+	if bullet_tick % 2 == 0:
+		gun = right_gun
+	else:
+		gun = left_gun
+	
+	var t = gun.global_transform
+	
+	# Apply slight input-based rotation (optional smoothing)
+	t.basis = t.basis.rotated(t.basis.z, roll_input * roll_speed * delta)
+	t.basis = t.basis.rotated(t.basis.x, pitch_input * pitch_speed * delta)
+	t.basis = t.basis.rotated(t.basis.y, yaw_input * yaw_speed * delta)
+	
+	return t.orthonormalized()
+	
+func bullet_handler(delta):
+	var bullet = bullet_scene.instantiate()
+	bullet_tick += 1
+	
+	bullet.global_transform = get_shoot_transform(delta)
+	
+	get_tree().current_scene.add_child(bullet)
+
 func _physics_process(delta: float):
 	
 	handle_acceleration(delta)
 	handle_rotation(delta)
+	
+	shoot_timer += delta
+	
+	if Input.is_action_pressed("shoot") and shoot_timer > shoot_at:
+		shoot_timer = 0
+		bullet_handler(delta)
 	
 	transform.basis = transform.basis.rotated(transform.basis.z, roll_input * roll_speed * delta)
 	transform.basis = transform.basis.rotated(transform.basis.x, pitch_input * pitch_speed * delta)
